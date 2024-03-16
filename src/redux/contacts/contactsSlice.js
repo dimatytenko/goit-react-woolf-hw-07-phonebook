6c1783603a0bb62e31fd0b951bfb718d7234d164
Dima Tytenko <amgpetronass@gmail.com>
2024-03-16 12:32:13 +0200

    fixed deploy

---
+++ src/redux/contacts/contactsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    addContact(state, action) {
+      state.items.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+export default contactsSlice.reducer;
